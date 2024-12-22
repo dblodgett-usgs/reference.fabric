bcecd3f5461bfb0bdb80170cc1ca3b5070439546
David Blodgett <dblodgett@usgs.gov>
2024-12-21 21:22:32 -0600

    fix: minor issue trying to run targets-waterbodies

--- R/targets-waterbodies.R
+++ R/targets-waterbodies.R
@@ -1,14 +1,15 @@
 #' Clean Waterbodies Target
-#' 
+#'
 #' @description
 #' Processes waterbodies according to steps 4.1.1 - 4.1.6.
-#' 
+#'
 #' @param waterbodies_path Path to unmodified NHDWaterbody file
 #' @param dir_cleaned Directory to output cleaned waterbody geometry
 #' @returns Path to cleaned waterbodies
 #'
 #' @export
 rf.targets.clean_waterbodies <- function(waterbodies_path, dir_cleaned) {
+
   wb <-
     sf::read_sf(waterbodies_path) |>
     dplyr::rename_with(~ toupper(.x), .cols = c(dplyr::everything(), -geometry)) |>
@@ -26,22 +27,27 @@ rf.targets.clean_waterbodies <- function(waterbodies_path, dir_cleaned) {
     # Remove Holes
     sfheaders::sf_remove_holes() |>
     # Ensure shapes are valid
-    sf::st_make_value() |>
+    # sf::st_make_valid() |>
     dplyr::left_join(sf::st_drop_geometry(wb), by = "COMID") |>
     dplyr::select(GNIS_ID, GNIS_NAME, COMID, FTYPE, geometry)
 
   wb_outfile <- file.path(dir_cleaned, rf.utils.extract_vpu(waterbodies_path), "waterbodies.fgb")
-  sf::st_write(wb_clean, wb_outfile, "waterbodies", delete_dsn = TRUE, quiet = TRUE)
+
+  dir.create(dirname(wb_outfile), recursive = TRUE, showWarnings = FALSE)
+
+  if(file.exists(wb_outfile)) unlink(wb_outfile)
+
+  sf::st_write(wb_clean, wb_outfile, "waterbodies", quiet = TRUE)
   wb_outfile
 }
 
 #' Process Reference Waterbodies Target
-#' 
+#'
 #' @description
 #' Processes _cleaned_ waterbodies according to steps 4.1.7 - 4.1.9.
-#' 
-#' @section FIXME: On/off network waterbodies based on NHD flowlines? 
-#' 
+#'
+#' @section FIXME: On/off network waterbodies based on NHD flowlines?
+#'
 #' @param wb_clean Path to cleaned waterbodies file
 #' @param wb_vpu The corresponding VPU of `wb_clean`
 #' @param wb_reference_dir Directory to output reference waterbodies
@@ -56,7 +62,7 @@ rf.targets.reference_waterbodies <- function(wb_clean, wb_vpu, wb_reference_dir)
     wb_clean |>
       dplyr::filter(is.na(GNIS_ID)) |>
       dplyr::mutate(member_comid = as.character(COMID))
-  
+
   # Implies that some GNIS_ID are not NA
   if (nrow(wb_filter) != nrow(wb_clean)) {
     wb_filter_2 <-
